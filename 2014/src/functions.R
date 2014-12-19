f97ce41d126b7b7b5faaca9dd52424f02509336e
Ryan Elmore <Ryan.Elmore@nrel.gov>
2014-12-18 17:23:19 -0700

    refactor team by year

--- 2014/src/functions.R
+++ 2014/src/functions.R
@@ -4,10 +4,17 @@
 
 
 GetNBASeasonTeamByYear <- function(team, year){
-  html <- paste(.base.api, team, "/", year, "_games.html", sep="")
-  stats <- readHTMLTable(html)[['teams_games']][c(1, 2, 4:6, 8:12)]
-  stats <- stats[-c(21, 42, 63, 84), ]
+  require(lubridate)
+  require(dplyr)
+  url <- paste(.base.api, team, "/", year, "_games.html", sep="")
+  stats <- readHTMLTable(url)[['teams_games']][c(1, 2, 6:8, 10:14)]
+  stats <- as.tbl(stats[-c(21, 42, 63, 84), ])
   stats[, c(1, 6:9)] <- apply(stats[, c(1, 6:9)], 2, as.numeric)
+  colnames(stats)[3] <- "Away_Indicator"
+  stats <- mutate(stats, Diff = Tm - Opp, 
+                         AvgDiff = cumsum(Diff)/G,
+                         Away = cumsum(Away_Indicator == '@'),
+                         BackToBack = c(NA, as.vector(diff(mdy(Date)))))
   stats$Diff <- stats$Tm - stats$Opp
   stats$AvgDiff <- cumsum(stats$Diff)/stats$G
   stats$Away <- cumsum(stats[, 3] == '@')
