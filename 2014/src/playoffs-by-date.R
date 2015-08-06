792aea3b2d756b04e8395f3243e762781a7532a2
Ryan Elmore <rtelmore@gmail.com>
2015-08-06 16:49:55 -0600

    playoffs by standings

--- 2014/src/playoffs-by-date.R
+++ 2014/src/playoffs-by-date.R
@@ -21,3 +21,14 @@ NBAStandingsByDate <- function(date_string){
   west <- html_table(r, fill = T)[[3]]
   return(list(East = east, West = west))
 }
+
+NBAPlayoffsFromStandings <- function(standings){
+  require(dplyr)
+  standings <- tbl_df(standings[-c(1, 7, 13), ]) %>%
+    mutate(Div = rep(1:3, each = 5))
+  names(standings)[c(1, 4)] <- c("Team", "Win_Pct")  
+  playoffs <- standings[seq(1, 26, by = 5)]
+  leftovers <- league[!(league$team %in% playoffs), ]
+  leftovers <- leftovers[ order(leftovers$conf, -leftovers$prob), ]
+  playoffs <- c(playoffs, leftovers$team[c(1:5, 13:17)])    
+}
