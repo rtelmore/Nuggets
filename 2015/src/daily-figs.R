80ea24c440bfdf65bd4a704cc5a9ac99824b4785
Ryan Elmore <rtelmore@gmail.com>
2015-11-11 09:51:15 -0700

    removed the b

--- 2015/src/daily-figs.R
+++ 2015/src/daily-figs.R
@@ -19,15 +19,15 @@ for(l in 1:kGames){
                    inv.logit((Nuggets[l, 4] - Nuggets[l, 5]*2)$fit)),
         lty = 1, lwd = 2, col = "navy")
 }
-points(1:kGames, inv.logit((Nuggets[, 4])$fit), pch = 16, cex = 2, 
+points(1:kGames, inv.logit((Nuggets[, 4])$fit), pch = 16, cex = 2,
        col = "grey")
-points(1:kGames, inv.logit((Nuggets[, 4])$fit), pch = 16, cex = 1, 
+points(1:kGames, inv.logit((Nuggets[, 4])$fit), pch = 16, cex = 1,
        col = "forestgreen")
 abline(h = .5, lty = 3, lwd = 2)
 dev.off()
 
 ## Overall Power Rankings
-pdf(file = paste("2015/fig/NBA-PR-", Sys.Date(), "-b.pdf", sep = ""),
+pdf(file = paste("2015/fig/NBA-PR-", Sys.Date(), ".pdf", sep = ""),
     height = 8.5,
     width = 11)
 
@@ -39,19 +39,19 @@ abline(v = 1:30, lty = 3, col = "lightgray")
 abline(h = .5, lty = 2)
 abline(v = (1:30)[pwr_rank$team == "DEN"], lty = 3, col = "darkred", lwd = 1.5)
 for(l in 1:30){
-  lines(c(l, l), c(inv.logit(c(pwr_rank[l, "fit"] + pwr_rank[l, "se"]*2)$fit), 
+  lines(c(l, l), c(inv.logit(c(pwr_rank[l, "fit"] + pwr_rank[l, "se"]*2)$fit),
                    inv.logit(c(pwr_rank[l, "fit"] - pwr_rank[l, "se"]*2)$fit)),
         lty = 1, lwd = 2, col = "navy")
 }
 points(1:30, inv.logit(pwr_rank %>% .$fit), pch = 16, cex = 2, col = "grey")
-points((1:30)[pwr_rank$conf == 1], 
-       inv.logit(pwr_rank %>% .$fit)[pwr_rank$conf == 1], 
+points((1:30)[pwr_rank$conf == 1],
+       inv.logit(pwr_rank %>% .$fit)[pwr_rank$conf == 1],
        pch = 16, cex = 1, col = "forestgreen")
-points((1:30)[pwr_rank$conf == 2], 
-       inv.logit(pwr_rank %>% .$fit)[pwr_rank$conf == 2], 
+points((1:30)[pwr_rank$conf == 2],
+       inv.logit(pwr_rank %>% .$fit)[pwr_rank$conf == 2],
        pch = 16, cex = 1, col = "darkorange")
 axis(1, at = 1:30, labels = pwr_rank$team, cex.axis = .65, las = 2)
-legend("topright", legend = c("West", "East"), pch = c(16, 16), 
+legend("topright", legend = c("West", "East"), pch = c(16, 16),
        col = c("darkorange", "forestgreen"), bg = "white")
 dev.off()
 
@@ -59,10 +59,10 @@ dev.off()
 
 nGames <- max(results$game)
 p <- ggplot(data = filter(results, conf == 1),
-            aes(x = game, y = inv.logit(fit), col = team, group = team, 
+            aes(x = game, y = inv.logit(fit), col = team, group = team,
                 ymin = inv.logit(fit - 2*se), ymax = inv.logit(fit + 2*se)))
 p <- ggplot(data = filter(results, conf == 1),
-            aes(x = game, y = inv.logit(fit), group = team, 
+            aes(x = game, y = inv.logit(fit), group = team,
                 ymin = inv.logit(fit - 2*se), ymax = inv.logit(fit + 2*se)))
 p + geom_line() +
   geom_errorbar(width = .25) +
@@ -76,8 +76,8 @@ ggsave(file = paste("2015/fig/East-PR-", Sys.Date(), ".pdf", sep = ""),
        width = 8.5)
 
 p <- ggplot(data = filter(results, conf == 2),
-            aes(x = game, y = inv.logit(fit), group = team, 
-                ymin = inv.logit(fit - 2*se), 
+            aes(x = game, y = inv.logit(fit), group = team,
+                ymin = inv.logit(fit - 2*se),
                 ymax = inv.logit(fit + 2*se)))
 p + geom_line() +
   geom_errorbar(width = .25) +
@@ -89,5 +89,3 @@ p + geom_line() +
 ggsave(file = paste("2015/fig/West-PR-", Sys.Date(), ".pdf", sep = ""),
        height = 11,
        width = 8.5)
-
-  
